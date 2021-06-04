Yesplan/babel-plugin-i18n

commit a9b37feeb908d0f79c882a1322ec986be94f8b04

Replaced generated expressions “props.intl” by “(props.intl ? props.intl : gintl)”, and “this.props.intl” by “(this && this.props && this.props.intl ? this.props.intl : gintl)” (thus allowing for a fallback to a, presumably globally defined, variable “gintl”).

src/helpers.js
@@ -1,5 +1,7 @@
 const generate = require("@babel/generator")["default"];
 
+const globalIntlIdentifier = "gintl";
+
 module.exports = {
   processFirstArgumentOfCall: function(path, t) {
     const firstArgument = path.get("arguments")[0];
@@ -106,17 +108,27 @@ module.exports = {
         path.scope.hasBinding("props") &&
         !(object && t.isThisExpression(object))
     ) {
+      const conditionExpression = t.MemberExpression(t.identifier("props"), t.identifier("intl"));
+      const propsIntlExpression = t.MemberExpression(t.identifier("props"), t.identifier("intl"));
       memberExpression = t.MemberExpression(
-          t.MemberExpression(t.identifier("props"), t.identifier("intl")),
+          t.conditionalExpression(conditionExpression, propsIntlExpression, t.identifier(globalIntlIdentifier)),
           t.identifier("formatMessage")
       );
     } else {
-      memberExpression = t.MemberExpression(
+      const conditionExpression = t.LogicalExpression("&&",
+        t.ThisExpression(),
+        t.LogicalExpression("&&",
+          t.MemberExpression(t.ThisExpression(), t.identifier("props")),
           t.MemberExpression(
-              t.MemberExpression(t.ThisExpression(), t.identifier("props")),
-              t.identifier("intl")
-          ),
-          t.identifier("formatMessage")
+            t.MemberExpression(t.ThisExpression(), t.identifier("props")),
+            t.identifier("intl"))));
+      const thisPropsIntlExpression = t.MemberExpression(
+        t.MemberExpression(t.ThisExpression(), t.identifier("props")),
+        t.identifier("intl")
+      );
+      memberExpression = t.MemberExpression(
+        t.conditionalExpression(conditionExpression, thisPropsIntlExpression, t.identifier(globalIntlIdentifier)),
+        t.identifier("formatMessage")
       );
     }
     if (firstArgument) {

test/fixtures/basic/output.js
@@ -24,44 +24,44 @@ function functionWithIntl(intl) {
 }
 
 function functionWithProps(props) {
-  props.intl.formatMessage({
+  (props.intl ? props.intl : gintl).formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   });
-  props.intl.formatMessage({
+  (props.intl ? props.intl : gintl).formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   }, []);
-  props.intl.formatMessage({
+  (props.intl ? props.intl : gintl).formatMessage({
     id: "translation message from {someone} about {subject}",
     defaultMessage: "translation message from {someone} about {subject}"
   }, {
     someone,
     subject
   });
-  props.intl.formatMessage({
+  (props.intl ? props.intl : gintl).formatMessage({
     id: "translation message from {0} about {1}",
     defaultMessage: "translation message from {0} about {1}"
   }, [someone, subject]);
 }
 
 function functionWithoutIntlAndProps() {
-  this.props.intl.formatMessage({
+  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   });
-  this.props.intl.formatMessage({
+  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   }, []);
-  this.props.intl.formatMessage({
+  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
     id: "translation message from {someone} about {subject}",
     defaultMessage: "translation message from {someone} about {subject}"
   }, {
     someone,
     subject
   });
-  this.props.intl.formatMessage({
+  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
     id: "translation message from {0} about {1}",
     defaultMessage: "translation message from {0} about {1}"
   }, [someone, subject]);
@@ -82,17 +82,17 @@ function functionWithMultipleCalls(intl, arg1, arg2) {
 }
 
 function functionWithDifferentThisScopes(props) {
-  props.intl.formatMessage({
+  (props.intl ? props.intl : gintl).formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   });
 
   const innerFunction = function (props) {
-    this.props.intl.formatMessage({
+    (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
       id: "translation in this scope",
       defaultMessage: "translation in this scope"
     });
-    props.intl.formatMessage({
+    (props.intl ? props.intl : gintl).formatMessage({
       id: "translation in props scope",
       defaultMessage: "translation in props scope"
     });

commit 00a2ec96f31cf3a9d741b6a0606a59cb321b79df

Fixed bug introduced in commit a9b37fe due to not taking into account that ‘babel-plugin-react-intl’ only extracts messages from ‘formatMessage’ calls that occur in expressions of the form ‘intl.formatMessage(…)’ and ‘𝓍.intl.formatMessage(…)’.

src/helpers.js
@@ -109,11 +109,12 @@ module.exports = {
         !(object && t.isThisExpression(object))
     ) {
       const conditionExpression = t.MemberExpression(t.identifier("props"), t.identifier("intl"));
-      const propsIntlExpression = t.MemberExpression(t.identifier("props"), t.identifier("intl"));
+      const intlObjectExpression = t.ObjectExpression([ t.ObjectProperty(t.identifier("intl"), t.identifier(globalIntlIdentifier)) ]);
       memberExpression = t.MemberExpression(
-          t.conditionalExpression(conditionExpression, propsIntlExpression, t.identifier(globalIntlIdentifier)),
-          t.identifier("formatMessage")
-      );
+        t.MemberExpression(
+          t.conditionalExpression(conditionExpression, t.identifier("props"), intlObjectExpression),
+          t.identifier("intl")),
+        t.identifier("formatMessage"));
     } else {
       const conditionExpression = t.LogicalExpression("&&",
         t.ThisExpression(),
@@ -122,14 +123,13 @@ module.exports = {
           t.MemberExpression(
             t.MemberExpression(t.ThisExpression(), t.identifier("props")),
             t.identifier("intl"))));
-      const thisPropsIntlExpression = t.MemberExpression(
-        t.MemberExpression(t.ThisExpression(), t.identifier("props")),
-        t.identifier("intl")
-      );
+      const thisPropsExpression = t.MemberExpression(t.ThisExpression(), t.identifier("props"));
+      const intlObjectExpression = t.ObjectExpression([ t.ObjectProperty(t.identifier("intl"), t.identifier(globalIntlIdentifier)) ]);
       memberExpression = t.MemberExpression(
-        t.conditionalExpression(conditionExpression, thisPropsIntlExpression, t.identifier(globalIntlIdentifier)),
-        t.identifier("formatMessage")
-      );
+        t.MemberExpression(
+          t.conditionalExpression(conditionExpression, thisPropsExpression, intlObjectExpression),
+          t.identifier("intl")),
+        t.identifier("formatMessage"));
     }
     if (firstArgument) {
       callArguments = [

test/fixtures/basic/output.js
@@ -24,44 +24,60 @@ function functionWithIntl(intl) {
 }
 
 function functionWithProps(props) {
-  (props.intl ? props.intl : gintl).formatMessage({
+  (props.intl ? props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   });
-  (props.intl ? props.intl : gintl).formatMessage({
+  (props.intl ? props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   }, []);
-  (props.intl ? props.intl : gintl).formatMessage({
+  (props.intl ? props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message from {someone} about {subject}",
     defaultMessage: "translation message from {someone} about {subject}"
   }, {
     someone,
     subject
   });
-  (props.intl ? props.intl : gintl).formatMessage({
+  (props.intl ? props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message from {0} about {1}",
     defaultMessage: "translation message from {0} about {1}"
   }, [someone, subject]);
 }
 
 function functionWithoutIntlAndProps() {
-  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
+  (this && this.props && this.props.intl ? this.props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   });
-  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
+  (this && this.props && this.props.intl ? this.props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   }, []);
-  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
+  (this && this.props && this.props.intl ? this.props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message from {someone} about {subject}",
     defaultMessage: "translation message from {someone} about {subject}"
   }, {
     someone,
     subject
   });
-  (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
+  (this && this.props && this.props.intl ? this.props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message from {0} about {1}",
     defaultMessage: "translation message from {0} about {1}"
   }, [someone, subject]);
@@ -82,17 +98,23 @@ function functionWithMultipleCalls(intl, arg1, arg2) {
 }
 
 function functionWithDifferentThisScopes(props) {
-  (props.intl ? props.intl : gintl).formatMessage({
+  (props.intl ? props : {
+    intl: gintl
+  }).intl.formatMessage({
     id: "translation message",
     defaultMessage: "translation message"
   });
 
   const innerFunction = function (props) {
-    (this && this.props && this.props.intl ? this.props.intl : gintl).formatMessage({
+    (this && this.props && this.props.intl ? this.props : {
+      intl: gintl
+    }).intl.formatMessage({
       id: "translation in this scope",
       defaultMessage: "translation in this scope"
     });
-    (props.intl ? props.intl : gintl).formatMessage({
+    (props.intl ? props : {
+      intl: gintl
+    }).intl.formatMessage({
       id: "translation in props scope",
       defaultMessage: "translation in props scope"
     });